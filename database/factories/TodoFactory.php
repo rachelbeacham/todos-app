980c8766ece17a301205d2785bb682a97ec88577
Rachel Beacham <rachelbeacham17@gmail.com>
2022-02-21 19:24:08 -0800

    Add todo table factory file.

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Todo;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Todo::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(4),
+            'completed' => false
+        ];
+    }
+}
