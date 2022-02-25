63186c1f6aadca14d5f6aa68557529807e681d46
Rachel Beacham <rachelbeacham17@gmail.com>
2022-02-25 14:11:03 -0800

    Update todos view to include a list of the todos and style with bootstrap.

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Todos</title>
 </head>
 <body>
-    <h1>Todos Page</h1>
-</body>
+    <div class="container">
+        <h1 class="text-center my-5">Todos Page</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Todos
+                    </div>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach($todos as $todo)
+                            <li class="list-group-item">
+                                {{$todo->name}}
+                                <button class="btn btn-primary btn-sm float-end">View</button>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </html>
