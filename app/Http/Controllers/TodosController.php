75227a954106e001120cbc52ba6c454fa356ba4f
Rachel Beacham <rachelbeacham17@gmail.com>
2022-02-25 14:49:33 -0800

    Add a method called show to the todos controller that returns a new view and finds a dynamic database id.

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -10,5 +10,11 @@ class TodosController extends Controller
     public function index() {
 
         return view('todos.index')->with('todos', Todo::all());
+
+    }
+    public function show($todoId) {
+
+        return view('todos.show')->with('todo', Todo::find($todoId));
+
     }
 }
