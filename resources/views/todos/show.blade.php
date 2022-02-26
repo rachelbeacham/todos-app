974a34c322a7fab42fd5b7bc37edfc1ab4913114
Rachel Beacham <rachelbeacham17@gmail.com>
2022-02-26 10:26:10 -0800

    Update existing views to remove common html and use the layout file instead.

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,28 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Todo Item</title>
-</head>
-<body>
-    <div class="container">
-        <h1 class="text-center my-5">
-            {{ $todo->name }}
-        </h1>
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-default">
-                    <div class="card-header text-center">Details</div>
-                    <div class="card-body">
-                        {{ $todo->description }}
-                    </div>
-                </div>
+@extends('layouts.app')
+
+@section('content')
+<h1 class="text-center my-5">
+    {{ $todo->name }}
+</h1>
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card card-default">
+            <div class="card-header text-center">
+                Details
+            </div>
+            <div class="card-body">
+                {{ $todo->description }}
             </div>
         </div>
     </div>
-</body>
-</html>
+</div>
+@endsection
