373826e9b7e1caea75b1697949cefed14ce72179
Rachel Beacham <rachelbeacham17@gmail.com>
2022-02-27 17:09:21 -0800

    Add an edit funtion to the todos controller that returns an edit view.  Add an update function that gets and validates the post data and saves it to the database.

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -42,5 +42,28 @@ class TodosController extends Controller
 
         // redirect user back to list of todos
         return redirect('/todos');
+
+    }
+    public function edit($todoId) {
+
+        $todo = Todo::find($todoId);
+        return view('todos.edit')->with('todo', $todo);
+
+    }
+    public function update($todoId) {
+
+        $this->validate(request(), [
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        $data = request()->all();
+        $todo = Todo::find($todoId);
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+
+        $todo->save();
+
+        return redirect('/todos');
     }
 }
